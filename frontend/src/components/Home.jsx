66fdef1b937298f49f5080f1d0853b37fcde7c51
Anthony Kummerfeldt <73972025+Kumm-feldt@users.noreply.github.com>
2025-09-07 21:19:54 -0500

    modify policy

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -25,7 +25,28 @@ const Home = () =>{
                 </div>
             </div>
             <img  className="cal-image" src={calendar} alt="" />
-        </div>
+
+     
+            </div>
+            <div className="instructions-wrapper">
+                <div className="list-div">
+                    <ul>
+                        <li className="item">
+                               <h2 className="headline">Number One</h2>
+                             <span>Add an Event with the Keyword "Shift" in your Google Calendar ie. "Shift [workplace]" </span>
+                        </li>
+                        <li className="item">
+                             <h2 className="headline">Number Two</h2>
+                             <span>Sign In/Login to UrShiftMate with your google account.</span>
+                        </li>
+                        <li className="item">
+                        <h2 className="headline">Number Three</h2>
+                             <span>Add your workplace name and hourly wage to our database</span>
+                        </li>
+                    </ul>
+                    
+                </div>
+            </div>  
         </>
     )
 }
