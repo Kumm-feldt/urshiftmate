c76b97aa31e8457fdc4d872d489e7c7974eb8945
Anthony Kummerfeldt <73972025+Kumm-feldt@users.noreply.github.com>
2025-09-10 23:58:27 -0500

    login css

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -9,17 +9,17 @@ const API_URL = import.meta.env.VITE_API_URL;
 function Login() {
   const googleAuthUrl = `${API_URL}/auth/google`;
 const formInput = <>        <div>
-            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="name@company.com" required />
         </div>
         <div>
-            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 ">Your password</label>
+            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
         </div>
         <div className="flex items-start">
             <div className="flex items-start">
                           </div>
-            <a href="#" className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+            <a href="#" className="ms-auto text-sm text-blue-700 hover:underline ">Lost Password?</a>
         </div></>
   return (<>
 
