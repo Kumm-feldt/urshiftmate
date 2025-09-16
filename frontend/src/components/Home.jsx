4f0ac364444f7d1c3cfcd8f7d2cc8fed35767d68
Anthony Kummerfeldt <73972025+Kumm-feldt@users.noreply.github.com>
2025-09-16 18:40:54 -0500

    modify steps

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,11 @@ import Header from "./Header";
 import "./Home.css"
 import calendar from "../assets/calendar.png"
 import { Link } from "react-router-dom";
+import stepOne from "../assets/step1.png"
+import stepThree from "../assets/step3.png"
+import stepFour from "../assets/step4.png"
+
+
 
 
 const Home = () =>{
@@ -29,20 +34,42 @@ const Home = () =>{
      
             </div>
             <div className="instructions-wrapper">
+
                 <div className="list-div">
+                    
                     <ul>
-                        <li className="item">
-                               <h2 className="headline">Number One</h2>
-                             <span>Add an Event with the Keyword "Shift" in your Google Calendar ie. "Shift [workplace]" </span>
-                        </li>
-                        <li className="item">
-                             <h2 className="headline">Number Two</h2>
-                             <span>Sign In/Login to UrShiftMate with your google account.</span>
-                        </li>
-                        <li className="item">
-                        <h2 className="headline">Number Three</h2>
-                             <span>Add your workplace name and hourly wage to our database</span>
-                        </li>
+                       <li className="item">
+                            <h2 className="headline">Step One</h2>
+                            <span>
+                                Add an event to your Google Calendar with the keyword <b>"Shift"</b>.  
+                                Example: <i>Shift Library</i>.
+                            </span>
+                            <img className='step-img' src={stepOne} alt="" />
+                            </li>
+                            <li className="item">
+                            <h2 className="headline">Step Two</h2>
+                            <span>
+                                Sign in to UrShiftMate using your Google account.
+                            </span>
+
+                            </li>
+                            <li className="item">
+                            <h2 className="headline">Step Three</h2>
+                            <span>
+                                In <b>Jobs & Calendar</b>, add your workplace name and hourly wage.
+                            </span>
+                            <img className='step-img' src={stepThree} alt="" />
+
+                            </li>
+                            <li className="item">
+                            <h2 className="headline">Step Four</h2>
+                            <span>
+                                Go to your <b>Dashboard</b> to view your expected paycheck.
+                            </span>
+                            <img className='step-img' src={stepFour} alt="" />
+
+                            </li>
+
                     </ul>
                     
                 </div>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -34,9 +34,8 @@ const formInput = <>        <div>
 
         <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to UrshiftMate</h5>
       
-        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
         <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+            Not registered? <a href={googleAuthUrl} className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
         </div>
         <div className="login-options text-sm font-medium">
             <p className="login-text">Or continue with</p>
