e2be5cc52ebaa74d04607387164d94094b1194b1
Anthony Kummerfeldt <73972025+Kumm-feldt@users.noreply.github.com>
2026-01-15 15:59:28 -0600

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,8 @@ const logger =  require("./logger.js")
 // database
 const db = require('./db.js')
 
-
 // Create the server
 const server = http.createServer(app);
-
-
 
 // Call it the same way, but only pass config in dev
 if (process.env.MODE === "dev") {
